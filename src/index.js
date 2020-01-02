eaf14f9aa45e933926f805b6e8bd4e5586aff39d
Oxana Howard <ovtyutcheva@gmail.com>
2020-01-02 11:37:31 -0800

    Added className to components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import './index.css';
 
 function Books(){
   return (
-    <section>
+    <section className="books">
+    <Book />
     <Book />
     <Book />
     <Book />
@@ -14,8 +16,9 @@ function Books(){
 
 const Book = () => {
   return (
-  <article>
-  <CoverImage /><Title />
+  <article className="book">
+  <CoverImage />
+  <Title />
   <Author />
 
   </article>
